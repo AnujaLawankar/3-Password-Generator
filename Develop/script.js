7c6e6555eb407e367f91b570ec2b6ae4dd78b48d
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-02-27 10:04:04 -0500

    Uploading code in HTML, CSS, JS, README files

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,127 @@
+
 // Assignment code here
+
+let password = "";
+
+
+//let passwordLenght = 8;
+const passwordCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+
+function generatePassword() {
+  console.log("Hey you click the button");
+  let continue_program = true;
+
+
+  //Ask for choice
+  var pwd_range = window.prompt("Please enter range of password(Range min=8, max=128)");
+  if (pwd_range == null) {
+    //breaking the loop
+    continue_program = false;
+  }
+
+  while ((pwd_range < 8 || pwd_range > 128) && (continue_program == true)) {
+    pwd_range = window.prompt("Invalid selection,\nPlease enter the password between range");
+
+
+  }
+
+  let typeSelection = 0;
+  let lowercase_selection = false;
+  let uppercase_selection = false;
+  let numeric_selection = false;
+  let specialchar_selection = false;
+
+  while (typeSelection == 0 && continue_program) {
+    let charType = "Do you want to include 'lowercase' in your password\nPress ok for 'yes' and cancle for 'no'";
+    if (confirm(charType) == true) {
+      typeSelection += 1;
+      lowercase_selection = true;
+    }
+
+    charType = "Do you want to include 'uppercase' in your password\nPress ok for 'yes' and cancle for 'no'";
+    if (confirm(charType) == true) {
+      typeSelection += 1;
+      uppercase_selection = true;
+    }
+
+    charType = "Do you want to include 'numeric' in your password\nPress ok for 'yes' and cancle for 'no'";
+    if (confirm(charType) == true) {
+      typeSelection += 1;
+      numeric_selection = true;
+    }
+
+    charType = "Do you want to include 'special charecter' in your password\nPress ok for 'yes' and cancle for 'no'";
+    if (confirm(charType) == true) {
+      typeSelection += 1;
+      specialchar_selection = true;
+    }
+    if (typeSelection == 0) {
+      let message = "You have to atleast select one char type!\nPlease try again!";
+      if (confirm(message) == true) {
+        //pass
+      } else {
+        continue_program = false;
+        break;
+      }
+    }
+
+  }
+
+  let upperCaseChar = passwordCharacters.slice(0, 25);
+  let lowerCaseChar = passwordCharacters.slice(26, 51);
+  let numericCaseChar = passwordCharacters.slice(52, 61);
+  let specialCaseChar = passwordCharacters.slice(62, 69);
+
+
+  let l_random = 0;
+  let u_random = 0;
+  let n_random = 0;
+  let s_random = 0;
+
+  while (password.length <= pwd_range - 1 && continue_program) {
+
+    if (lowercase_selection && password.length <= pwd_range - 1) {
+      l_random = Math.floor(Math.random() * lowerCaseChar.length);
+      password = password.concat(lowerCaseChar.charAt(l_random));
+
+    }
+
+    if (uppercase_selection && password.length <= pwd_range - 1) {
+      u_random = Math.floor(Math.random() * upperCaseChar.length);
+      password = password.concat(upperCaseChar.charAt(u_random));
+    }
+
+    if (numeric_selection && password.length <= pwd_range - 1) {
+      n_random = Math.floor(Math.random() * numericCaseChar.length);
+      password = password.concat(numericCaseChar.charAt(n_random));
+    }
+
+    if (specialchar_selection && password.length <= pwd_range - 1) {
+      s_random = Math.floor(Math.random() * specialCaseChar.length);
+      password = password.concat(specialCaseChar.charAt(s_random));
+    }
+    console.log(password);
+  }
+  //alert("Thanks for choosing")
+
+  /*
+    if (pwd_range <= 8 && pwd_range >= 128) {
+      window.prompt("Thank you for choosing");
+    } else {
+      window.prompt("Enter the password between range");
+  
+    }
+  */
+  //var pwd_char = window.prompt("Choose up")
+  if (password.length > 0) {
+    alert("Password is generated in the below text area!");
+  } else {
+    alert("Password is not created\nPlease try again by reloading the page!");
+  }
+  return password;
+
+}
 
 
 // Get references to the #generate element
@@ -15,3 +138,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
--- README.md
+++ README.md
@@ -1,1 +1,93 @@
 # Password Generator Starter Code
+
+## Description
+ In Password Generator web application the password is generated accroding to the input certiria of the user. There are many options for the user to select the password cretiria like,
+ 
+1. First enter the length of password. The length of password should be at least 8 characters and no more than 128 characters.
+
+2. If the user enter the out of range length. Then the password generator show the alert message of "Invalid selection, Please enter the password between range".
+
+3. When the user enter the between the range then the next prompt displayed for selection of the lowercase characters.
+
+4. If user want to add lowercase characters in their password than they can select "Ok" button or exclude lowercase characters in their password than they can select "Cancle".
+
+5. User can select "ok" or "cancle" button for the first selection of lowercase.
+
+6. Next, selection for uppercase characters. If user want to add uppercase characters in their password than they can select "Ok" button or exclude uppercase characters in their password than they can select "Cancle".
+   
+7. Next, selection for numeric characters. If user want to add special characters in their password than they can select "Ok" button or exclude numeric characters in their password than they can select "Cancle".
+
+8.  Next, selection for specialcase characters. If user want to add number in their password than they can select "Ok" button or exclude special case characters in their password than they can select "Cancle".
+   
+9. If the user selected all the "Cancle" button among the selection certiria. Then the alert message display "Please select atleast one characters".
+
+10. If the user selected any of the among the selection certiria. Then accroding to that certiria the password will be generated. The password is generated as long the password range input.
+
+11. The generated password will be displayed in the web application.
+
+
+
+
+## URLs:-
+Here, you can find the webpage.
+Paswword generator web application's URL:- 
+
+
+Here, you can find the GitHub url.
+
+GitHub URL:- 
+
+
+## Installation
+
+N/A
+
+## Usage
+
+N/A
+
+## Credits
+
+N/A
+
+## License
+
+N/A
+
+## Assets
+
+The following image demonstrates the website appearance:
+1. Taking password range input.
+![Website](./Develop/assets/screenshot1.png)
+
+
+2. Asking user for include lowercase characters.
+![Website](./Develop/assets/screenshot2.png)
+
+
+3.Asking user for include uppercase characters.
+![Website](./Develop/assets/screenshot3.png)
+
+
+4. Asking user for include numeric characters.
+![Website](./Develop/assets/screenshot4.png)
+
+
+5. Asking user for include specialcase characters.
+![Website](./Develop/assets/screenshot5.png)
+
+
+6. Password generated according to selection certiria,   Password generated as long the password range in input.
+![Website](./Develop/assets/scrennshot6.png)
+
+
+7. Password generated
+![Website](./Develop/assets/screenshot7.png)
+
+
+8. Validation:- User enter out of password range input.
+![Website](./Develop/assets/screenshot8.png)
+
+
+9. Error message and asking to re-enter password range input.
+![Website](./Develop/assets/screenshot9.png)
